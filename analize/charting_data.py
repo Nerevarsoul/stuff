ba60c2dcbf9a3ea023c07e84fce35d52141bb2fd
neri <kesha_everywhere@mail.ru>
2016-05-24 18:09:19 +0200

    fixing

--- analize/charting_data.py
+++ analize/charting_data.py
@@ -4,29 +4,21 @@ import pandas as pd
 
 from matplotlib.dates import MinuteLocator, HourLocator,\
     DayLocator, MONDAY, DateFormatter, WeekdayLocator
-
+from matplotlib.finance import quotes_historical_yahoo_ohlc, candlestick_ohlc
 
 df = pd.read_json('for_monthly_charts.json',)
 for i in range(1):  # len(df.columns)):
     price = pd.DataFrame(df.ix[1,i])
     price['DT'] = pd.to_datetime(price['DT'])
-
-    fig, ax1 = plt.subplots()
-    fig.set_size_inches((15, 8))
-    ax1.plot_date(price["DT"], price["C"], '-')
-
-    #ax2 = ax1.twinx()
-    #ax2.plot_date(price["DT"], price["L"], '-', label='L')
-
-    #ax3 = ax1.twinx()
-    #ax3.plot_date(price["DT"], price["H"], '-', label='H')
-
-    #ax4 = ax1.twinx()
-    #ax4.plot_date(price["DT"], price["O"], '-', label='O')
-
-    #ax5 = ax1.twinx()
-    #ax5.plot_date(price["DT"], price["Vol"], '-', label='Vol')
     
+    fig = plt.figure()
+    ax1 = plt.subplot2grid((1, 1), (0, 0))
+    ax1.plot_date(price['DT'], price['O'],'-')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
     name = "{}-{}.png".format(price['DT'][0].year, price['DT'][0].month)
+    plt.title(name) 
+    plt.legend()   
+
     plt.savefig(name)
 
