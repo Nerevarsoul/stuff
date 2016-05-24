132aa24f367b5af4695b0cae51a17f22d6f3d192
neri <kesha_everywhere@mail.ru>
2016-05-24 17:45:24 +0200

    fix

--- analize/charting_data.py
+++ analize/charting_data.py
@@ -11,17 +11,22 @@ for i in range(1):  # len(df.columns)):
     price = pd.DataFrame(df.ix[1,i])
     price['DT'] = pd.to_datetime(price['DT'])
 
-    # fig = plt.figure()
-    # ax1 = plt.subplot2grid((1,1), (0,0))
-    fig, ax = plt.subplots()
-    ax.plot_date(price["DT"], price["C"],'-')
-    ax.grid(True)
-    # for label in ax.xaxis.get_ticklabels():
-        # label.set_rotation(45)
+    fig, ax1 = plt.subplots()
+    fig.set_size_inches((15, 8))
+    ax1.plot_date(price["DT"], price["C"], '-', label='C')
 
-    # x_min = min(price["DT"])
-    # x_max = max(price["DT"])
-    # plt.xticks(np.arange(x_min.day, x_max.day, 1.0))
+    ax2 = ax1.twinx()
+    ax2.plot_date(price["DT"], price["L"], '-', label='L')
+
+    ax3 = ax1.twinx()
+    ax3.plot_date(price["DT"], price["H"], '-', label='H')
+
+    ax4 = ax1.twinx()
+    ax4.plot_date(price["DT"], price["O"], '-', label='O')
+
+    ax5 = ax1.twinx()
+    ax5.plot_date(price["DT"], price["Vol"], '-', label='Vol')
+    
     name = "{}-{}.png".format(price['DT'][0].year, price['DT'][0].month)
     plt.savefig(name)
 
