73f3d71024ade0cde8eaf709a7b56dfec139b2f0
neri <kesha_everywhere@mail.ru>
2016-05-24 18:22:00 +0200

    fixing

--- analize/charting_data.py
+++ analize/charting_data.py
@@ -2,17 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from matplotlib.dates import MinuteLocator, HourLocator,\
-    DayLocator, MONDAY, DateFormatter, WeekdayLocator
-from matplotlib.finance import quotes_historical_yahoo_ohlc, candlestick_ohlc
 
 df = pd.read_json('for_monthly_charts.json',)
 for i in range(1):  # len(df.columns)):
     price = pd.DataFrame(df.ix[1,i])
     price['DT'] = pd.to_datetime(price['DT'])
     
-    fig = plt.figure()
-    fig.set_size_inches((335, 18))
+    fig = plt.figure(figsize=(40, 20))
     ax1 = plt.subplot2grid((1, 1), (0, 0))
     ax1.plot_date(price['DT'], price['O'],'-')
     for label in ax1.xaxis.get_ticklabels():
