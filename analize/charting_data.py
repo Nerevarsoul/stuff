96333cee9494223c1794b24726483f304b42fbd0
neri <kesha_everywhere@mail.ru>
2016-05-25 16:10:59 +0200

    candle

--- analize/charting_data.py
+++ analize/charting_data.py
@@ -1,23 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
+from pylab import date2num
+from matplotlib.dates import MinuteLocator, HourLocator,\
+    DayLocator, MONDAY, DateFormatter, WeekdayLocator
+from matplotlib.finance import quotes_historical_yahoo_ohlc, candlestick_ohlc
 
 df = pd.read_json('for_monthly_charts.json',)
 for i in range(1):  # len(df.columns)):
     price = pd.DataFrame(df.ix[1,i])
     price['DT'] = pd.to_datetime(price['DT'])
+    price['num_date'] = price['DT'].apply(date2num)
     
-    fig = plt.figure(figsize=(40, 20))
-    ax1 = plt.subplot2grid((1, 1), (0, 0))
-    ax1.plot_date(price['DT'], price['O'],'-')
-    for label in ax1.xaxis.get_ticklabels():
-        label.set_rotation(45)
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    name = "{}-{}.png".format(price['DT'][0].year, price['DT'][0].month)
-    plt.title(name) 
-    plt.legend()   
+    dayFormatter = DateFormatter('%d')
+    quotes = price[['num_date','O','L','H','C']].dropna()
 
+    fig, ax = plt.subplots()
+    fig.set_size_inches((15,8))
+    ax.xaxis.set_major_formatter(dayFormatter)
+    candlestick_ohlc(ax, quotes.values, width=0.05,colorup='g',colordown='r')
+    ax.xaxis_date()
+    ax.autoscale_view()
+    plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+    name = "{}-{}.png".format(price['DT'][0].year, price['DT'][0].month)
     plt.savefig(name)
 
