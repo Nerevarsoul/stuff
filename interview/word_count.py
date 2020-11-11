a315cecf0d4d115c165dab962be86ead7746953f
Innokentiy <keshaeverywhere@gmail.com>
2020-11-11 22:44:34 +0300

    add defaultdict realisation

--- interview/word_count.py
+++ interview/word_count.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
 import sys
-from collections import Counter
+from collections import Counter, defaultdict
 
 
-def print_word(result):
-    for word, frequency in sorted(result.most_common(), key=lambda res: (-res[1], res[0])):
+def print_word(result: Counter):
+    for word, frequency in result.most_common():
         print('<{}>: <{}>'.format(word, frequency))
 
 
@@ -21,6 +20,28 @@ def main():
     try:
         result = word_count()
         print_word(result)
+    except IOError:
+        print('File with {} does not exist'.format(filename))
+
+
+def word_count_defaultdict():
+    result = defaultdict(lambda: 0)
+    with open(filename, 'r') as text_file:
+        for line in text_file:
+            for word in line.split():
+                result[word] += 1
+    return result
+
+
+def print_word_defaultdict(result: defaultdict):
+    for word, frequency in sorted(result.items(), key=lambda res: (-res[1], res[0])):
+        print(f'<{word}>: <{frequency}>')
+
+
+def main_defaultdict():
+    try:
+        result = word_count_defaultdict()
+        print_word_defaultdict(result)
     except IOError:
         print('File with {} does not exist'.format(filename))
 
